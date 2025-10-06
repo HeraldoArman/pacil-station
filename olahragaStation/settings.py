d110e243f9a4785c80696482bdbe9c7775a7b441
HeraldoArman <heraldoarman7@gmail.com>
2025-10-06 21:29:39 +0700

    feat: challenge

--- olahragaStation/settings.py
+++ olahragaStation/settings.py
@@ -138,7 +138,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static' # merujuk ke /static root project pada mode development
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
